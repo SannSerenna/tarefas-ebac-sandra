ac2bc2f8d9ff0197fdf0bffd8a3d3a1a8be62f45
SannSerenna <166335997+SannSerenna@users.noreply.github.com>
2024-08-27 19:42:47 -0300

    Annotation
    Instruções:
    
    Para rodar o programa, clique com o botão direito na classe AnnotationExample no Project Explorer e selecione Run 'AnnotationExample.main()':
    Nome da Tabela: minha_tabela

---
+++ anotação/Mod18/src/MinhaClasse.java
@@ -0,0 +1,26 @@
+@Tabela(nome = "minha_tabela")
+public class MinhaClasse {
+    private int id;
+    private String nome;
+
+    public MinhaClasse(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
